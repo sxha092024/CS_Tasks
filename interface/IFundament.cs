b81ab58104ef4406e024a8db3a82fd29051b46ff
JorgenMJobloop <jorgen@jobloop.no>
2024-10-17 10:27:42 +0200

    inital commit

---
+++ interface/IFundament.cs
@@ -0,0 +1,27 @@
+public interface IFundament
+{
+    /// <summary>
+    /// Sums two numbers together, in void, we can specify if these numbers are integers, doubles or long
+    /// </summary>
+    void SumTwoNumbers();
+
+    /// <summary>
+    /// sums together two numbers that it recieves as parameter arguments
+    /// </summary>
+    /// <param name="a">the value of a</param>
+    /// <param name="b">the value of b</param>
+    /// <returns>a + b</returns>
+    double SumTwoNumbersOfGivenType(double a, double b);
+
+    /// <summary>
+    /// check whether a number is even (divisible by two) or odd
+    /// </summary>
+    void CheckOddOrEven();
+
+    /// <summary>
+    /// A method that returns a interesting fact, that the user of the program can define as a parameter argument
+    /// </summary>
+    /// <param name="fact">a interesting fact</param>
+    /// <returns>a fact of type: string</returns>
+    string GetAInterestingFact(string fact);
+}
---
+++ interface/IGreetings.cs
@@ -0,0 +1,15 @@
+public interface IGreetings
+{
+    // english greeting
+    void SayHello();
+
+    // norwegian greeting
+    void SiHei();
+
+    // german greeting
+    void SagHallo();
+    // spanish greeting
+    void DecirHola();
+    // japanese greeting
+    void YoiTsuitachi();
+}
