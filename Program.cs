53001c500e0729c14150101beebda8dc04bb1b9e
JorgenMJobloop <jorgen@jobloop.no>
2024-10-17 10:28:17 +0200

    inital commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CS_Tests;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create a new instance of the Greeting class
+        Greetings greetings = new Greetings();
+        // English greeting
+        greetings.SayHello();
+        // Japanese greeting
+        greetings.YoiTsuitachi();
+
+    }
+}
