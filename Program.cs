922eea09134c227816c1c12199eb0c5c78506a08
sxha092024 <183362499+sxha092024@users.noreply.github.com>
2024-10-17 13:00:57 +0200

    Solution

--- Program.cs
+++ Program.cs
@@ -4,12 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Create a new instance of the Greeting class
-        Greetings greetings = new Greetings();
-        // English greeting
-        greetings.SayHello();
-        // Japanese greeting
-        greetings.YoiTsuitachi();
+        try
+        {
+            // Create a new instance of the Greeting class
+            Greetings greetings = new Greetings();
+            // English greeting
+            greetings.SayHello();
+            // Japanese greeting
+            greetings.YoiTsuitachi();
 
+            Fundament fundament = new Fundament();
+
+            fundament.CheckOddOrEven(1); // odd
+            fundament.CheckOddOrEven(2); // even
+
+            fundament.SumTwoNumbersOfGivenType(1, 2); // 3
+
+
+            fundament.SumTwoNumbers(); // user input
+
+            string fact = fundament.GetAInterestingFact("Factoid actually means, a statement presented as a fact, which is false");
+            Console.WriteLine(fact);
+
+            string rev = fundament.ReverseString(fact);
+            Console.WriteLine(rev);
+
+            fundament.PrintFibonnaciSeries(); // 1836311903 before overflowing of the int type
+
+            int factorial = fundament.RecursiveFactorial(6); // 720
+            Console.WriteLine($"{factorial}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            Environment.Exit(-1);
+        }
     }
 }
--- classes/Fundament.cs
+++ classes/Fundament.cs
@@ -1,23 +1,87 @@
+
+using System.Globalization;
+using System.Numerics;
+
 public class Fundament : IFundament
 {
     public void CheckOddOrEven(double a)
     {
-        throw new NotImplementedException();
+        double odd_or_even = a % 2;
+
+        if (odd_or_even == 0)
+        {
+            Console.WriteLine($"{a} is even");
+        }
+        else
+        {
+            Console.WriteLine($"{a} is odd, remainder after modulo = {odd_or_even}");
+        }
     }
 
     public string GetAInterestingFact(string fact)
     {
-        throw new NotImplementedException();
+
+        return $"Did you know? You passed in: '{fact}'. Neat right?";
     }
 
+    /// <summary>
+    /// Sums two numbers together.
+    /// </summary>
+    /// <exception cref="ArgumentException"></exception>
     public void SumTwoNumbers()
     {
-        throw new NotImplementedException();
+        // usage of dynamic here is due to the inability to do late binding
+        // of the type information with the usage of the `var` variable declaration.
+        // As such, resorting to dynamic to fulfill the interface requirements of being able to 
+        // chose the underlying numerical types of int, long, or double.
+        // (There may be a way to achieve this without the usage of dynamic but eeeeeehhhhhh)
+        dynamic num1;
+        dynamic num2;
+        AskForNum(out num1);
+        AskForNum(out num2);
+
+        var sum = num1 + num2;
+        Console.WriteLine($"Sum of {num1} + {num2} = {sum}");
+    }
+
+    private void AskForNum(out dynamic num)
+    {
+        Console.WriteLine("Specify number type: int, long, or double");
+        string? num_type = Console.ReadLine();
+        switch (num_type)
+        {
+            case "int":
+                num = GetNum<int>();
+                break;
+            case "long":
+                num = GetNum<long>();
+                break;
+            case "double":
+                num = GetNum<double>();
+                break;
+            default:
+                throw new ArgumentException($"Cannot resolve `{num_type}` to int, long, or double");
+        }
+    }
+
+    private T GetNum<T>()
+    where T : IParsable<T>, INumber<T>
+    {
+        Console.WriteLine("Enter a number");
+        string? number = Console.ReadLine();
+        if (number != null)
+        {
+            return T.Parse(number, NumberStyles.Number, provider: null);
+        }
+        else
+        {
+            throw new ArgumentException($"Cannot parse `{number}` as a number");
+        }
     }
 
     public double SumTwoNumbersOfGivenType(double a, double b)
     {
-        throw new NotImplementedException();
+        return a + b;
     }
 
 
@@ -25,33 +89,47 @@ public class Fundament : IFundament
 
     public void PrintFibonnaciSeries()
     {
-        // TODO: Implement the Fibonacci sequence
+        Console.WriteLine("Fibonnaci Sequence");
         int a = 0;
         int b = 1;
-        int c;
+        int c = a + b;
 
-        /* 
-        c = a + b
-        a = b
-        b = c
-        */
+        Console.Write($"{c}");
+
+        while (c >= 0)
+        {
+            try
+            {
+                checked
+                {
+                    c = a + b;
+                    Console.Write($", {c}");
+                }
+            }
+            catch (OverflowException)
+            {
+                break;
+            }
+            a = b;
+            b = c;
+        }
+
+        Console.WriteLine("\nDatatype has overflowed");
     }
 
     public string ReverseString(string str)
     {
-        // TODO: reverse the given string
-        return "";
+        return new string(str.Reverse().ToArray());
     }
 
     public int RecursiveFactorial(int n)
     {
-        // Todo: Implement a recursive method(a method that calls itself within its own function scope)
         // To Check the factorial of a number(norsk: Fakultet) we look at n!, where n = n * 1 * 2 * 3...
         if (n == 0)
         {
             return 1;
         }
-        return n;
+        return n * RecursiveFactorial(n - 1);
     }
 
 
